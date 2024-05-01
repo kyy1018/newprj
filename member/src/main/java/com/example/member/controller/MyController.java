6a9d79634a35b4c393b62009ceb3260834d1077c
Kim ye young <kimyyoung1018@gmail.com>
2024-05-01 15:46:58 +0900

    111

--- member/src/main/java/com/example/member/controller/MyController.java
+++ member/src/main/java/com/example/member/controller/MyController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.member.dao.IMemberDao;
+
 @Controller
 public class MyController {
 	@Autowired
